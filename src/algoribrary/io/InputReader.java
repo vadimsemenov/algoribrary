f0e1e126c26e2cc3ce9b0800377746bd97a19a96
Vadim Semenov <vadim.pillarbox@gmail.com>
2014-03-15 12:06:49 +0400

    probcommit v2

---
+++ src/algoribrary/io/InputReader.java
@@ -0,0 +1,62 @@
+package algoribrary.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+/**
+ * Created by vadim on 15 Mar 2014.
+ */
+public class InputReader {
+    private final BufferedReader reader;
+    private StringTokenizer tokenizer;
+
+    public InputReader(InputStream in) {
+        reader = new BufferedReader(new InputStreamReader(in));
+    }
+
+    public int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    public long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    public double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    public BigInteger nextBigInteger() {
+        return new BigInteger(next());
+    }
+
+    public boolean hasNext() {
+        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            try {
+                String line = reader.readLine();
+                if (line == null) {
+                    return false;
+                }
+                tokenizer = new StringTokenizer(line);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return true;
+    }
+
+    public String next() {
+        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            try {
+                tokenizer = new StringTokenizer(reader.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return tokenizer.nextToken();
+    }
+}
